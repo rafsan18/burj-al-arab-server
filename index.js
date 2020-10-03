30cc550402763425198cb9b63c5511ff042105e9
rafsan <rafsanamin18@gmail.com>
2020-10-03 06:17:04 +0600

    49.7

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const admin = require("firebase-admin");
+
 const port = 5000;
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+const serviceAccount = require("./burj-al-arab-dc701-firebase-adminsdk-pwhdk-126df3b9a2.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://burj-al-arab-dc701.firebaseio.com",
+});
 
 const password = "aZXjq38M9CEbx86";
 
@@ -26,7 +35,7 @@ app.get("/", (req, res) => {
 
 client.connect((err) => {
     const bookings = client.db("burjAlArab").collection("bookings");
-    console.log("db connected successfully");
+    //console.log("db connected successfully");
     // create
     app.post("/addBooking", (req, res) => {
         const newBooking = req.body;
@@ -38,10 +47,29 @@ client.connect((err) => {
     // read
 
     app.get("/bookings", (req, res) => {
-        console.log(req.query.email);
-        bookings.find({ email: req.query.email }).toArray((err, documents) => {
-            res.send(documents);
-        });
+        const bearer = req.headers.authorization;
+        if (bearer && bearer.startsWith("Bearer ")) {
+            const idToken = bearer.split(" ")[1];
+            console.log({ idToken });
+            admin
+                .auth()
+                .verifyIdToken(idToken)
+                .then(function (decodedToken) {
+                    const tokenEmail = decodedToken.email;
+                    const queryEmail = req.query.email;
+                    console.log({ tokenEmail, queryEmail });
+                    if (tokenEmail == req.query.email) {
+                        bookings
+                            .find({ email: req.query.email })
+                            .toArray((err, documents) => {
+                                res.send(documents);
+                            });
+                    }
+                })
+                .catch(function (error) {
+                    // Handle error
+                });
+        }
     });
 });
 
